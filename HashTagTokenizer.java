5154c8043246504e9de74a468d980a1906c9ec63
YR <you@example.comyarinraichman@gmail.com>
2024-02-07 12:38:04 +0200

    Uploading HW07 - 1st try

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -1,40 +1,72 @@
 
-
 public class HashTagTokenizer {
 
 	public static void main(String[] args) {
 
 		String hashTag = args[0];
-		String []dictionary = readDictionary("dictionary.txt");
+		String[] dictionary = readDictionary("dictionary.txt");
 		breakHashTag(hashTag, dictionary);
 	}
 
+	// return an array that contains all 3000 words in dictionary.txt.
 	public static String[] readDictionary(String fileName) {
-		String[] dictionary = new String[3000];
-
-		In in = new In(fileName);
-
-		// Your code here
-
+		String[] dictionary = new String[3000]; // array of strings that will contain all words in dictionary.txt.
+		In in = new In(fileName); // open file.
+		// goes throu the whole dictionary txt file of 3000 lines.
+		for (int i = 0; i < dictionary.length; i++) {
+			// save each line in and array of strings.
+			dictionary[i] = in.readString();
+		}
+		// return array of strings that contains all of 3000 words in dictionary.txt.
 		return dictionary;
 	}
 
-	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+	// checks if word is in dictionary
+	public static boolean existInDictionary(String word, String[] dictionary) {
+		int flag = 0; // indicator if the whole word if in the dictionary.
+		// go throu all of the dictionary to seek word.
+		for (int i = 0; i < dictionary.length; i++) {
+			// check if the lengths are equal (if a char by char check is needed).
+			if (dictionary[i].length() == word.length()) {
+				// go through each char in word and dictionary.
+				for (int j = 0; j < dictionary[i].length(); j++) {
+					if (word.charAt(j) == dictionary[i].charAt(j)) {
+						flag++;
+					}
+					// check if all of the characters are equal(value and position)
+					if (flag == (word.length())) {
+						return true;
+					}
+				}
+			}
+			// reseting flag (checking a new word in dictionary).
+			flag = 0;
+		}
+		// if word is not found return false.
+		return false;
 	}
 
+	// print all words recognized in hashtag from dictionary with a recursive
+	// implimantation.s
 	public static void breakHashTag(String hashtag, String[] dictionary) {
-
 		// Base case: do nothing (return) if hashtag is an empty string.
-        if (hashtag.isEmpty()) {
-            return;
-        }
- 
-        int N = hashtag.length();
-
-        for (int i = 1; i <= N; i++) {
-		
-        }
-    }
+		if (hashtag.isEmpty()) {
+			return;
+		}
+		int N = hashtag.length(); // lenght of the string given.
+		hashtag = hashtag.toLowerCase(); // turns hashtag into lowercase characters.
+		// go thoru all chars and implement a recursiv solution for searching each word
+		// identified in dictionary.txt.
+		for (int i = 1; i <= N; i++) {
+			// check if hashtag is in dictionary.
+			if (existInDictionary(hashtag.substring(0, i), dictionary)) {
+				// print if the word is recognzed.
+				System.out.println(hashtag.substring(0, i));
+				// search for more words that apear after the found word.
+				breakHashTag(hashtag.substring(i, N), dictionary);
+				return;
+			}
+		}
+	}
 
 }
--- SpellChecker.java
+++ SpellChecker.java
@@ -1,7 +1,6 @@
 
 public class SpellChecker {
 
-
 	public static void main(String[] args) {
 		String word = args[0];
 		int threshold = Integer.parseInt(args[1]);
@@ -10,26 +9,61 @@ public class SpellChecker {
 		System.out.println(correction);
 	}
 
+	// returns str without its first char.
 	public static String tail(String str) {
-		// Your code goes here
+		// check if str has a tail.
+		if (str.length() == 1) {
+			return "";
+		}
+		return str.substring(1);
 	}
 
+	// returns the first char.
+	public static char head(String str) {
+		return str.charAt(0);
+	}
+
+	// implementation of Levinshrein program recursivly.
 	public static int levenshtein(String word1, String word2) {
-		// Your code goes here
+		// base case - at least on of the strings is empty.
+		if (word1.isEmpty()) {
+			return word2.length();
+		}
+		if (word2.isEmpty()) {
+			return word1.length();
+		}
+		if (head(word1) == head(word2)) {
+			return levenshtein(tail(word1), tail(word2));
+		}
+		// return the min between deletion, insertion and substitution count.
+		return 1 + Math.min(levenshtein(tail(word1), word2),
+				Math.min(levenshtein(word1, tail(word2)), levenshtein(tail(word1), tail(word2))));
 	}
 
 	public static String[] readDictionary(String fileName) {
-		String[] dictionary = new String[3000];
-
-		In in = new In(fileName);
-
-		// Your code here
-
+		String[] dictionary = new String[3000]; // array of strings that will contain all words in dictionary.txt.
+		In in = new In(fileName); // open file.
+		// goes throu the whole dictionary txt file of 3000 lines.
+		for (int i = 0; i < dictionary.length; i++) {
+			// save each line in and array of strings.
+			dictionary[i] = in.readString();
+		}
+		// return array of strings that contains all of 3000 words in dictionary.txt.
 		return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
-		// Your code goes here
+		// go through the dictionary.
+		for (int i = 0; i < dictionary.length; i++) {
+			// find matching first letters.
+			if (word.charAt(0) == dictionary[i].charAt(0)) {
+				// find the first word with given threshold or less.
+				if (levenshtein(word, dictionary[i]) <= threshold) {
+					return dictionary[i];
+				}
+			}
+		}
+		return word;
 	}
 
 }
